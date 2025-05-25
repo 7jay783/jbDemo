3c4bfd429c2eb70bac691badc3c483da8a27fa79
Jay Prakash <7jay783@gmail.com>
2025-05-25 20:10:11 +0530

    git added

---
+++ src/main/java/com/jbdemo/jbdemo/JbdemoApplication.java
@@ -0,0 +1,13 @@
+package com.jbdemo.jbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbdemo/jbdemo/JbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbdemo.jbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
